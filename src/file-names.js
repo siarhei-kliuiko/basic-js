82cc16a8046f30f1865a84fc2ef5f6464b40ceea
siarhei-kliuiko <siarhei.kliuiko@gmail.com>
2023-04-15 20:44:08 +0300

    feat: implement renameFiles function

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const newFilenames = Array(names.length);
+  for (let i = 0; i < names.length; i++) {
+    let isNameUsed = false;
+    for (let j = i - 1; j >= 0; j--) {
+      if (names[i] === newFilenames[j]) {
+        newFilenames[i] = names[i] + "(1)";
+        isNameUsed = true;
+        break;
+      } else if (newFilenames[j].startsWith(names[i])) {
+        const usedNumber = newFilenames[j].match(/\(\d+\)$/);
+        if (usedNumber && names[i] === newFilenames[j].replace(usedNumber[0], "")) {
+          newFilenames[i] = `${names[i]}(${Number(usedNumber[0].slice(1, -1)) + 1})`;
+          isNameUsed = true;
+          break;
+        }
+      }
+    }
+
+    if (!isNameUsed) {
+      newFilenames[i] = names[i];
+    }
+  }
+
+  return newFilenames;
 }
 
 module.exports = {
