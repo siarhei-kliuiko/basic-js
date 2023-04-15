333384abe06b77422af5aee1a073946bd48c423e
siarhei-kliuiko <siarhei.kliuiko@gmail.com>
2023-04-15 18:04:51 +0300

    feat: implement getDNSStats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsList = [];
+  domains.forEach(domain => {
+    const domainParts = domain.split(".").reverse();
+    let domainPart = "";
+    for (let i = 0; i < domainParts.length; i++) {
+      domainPart += `.${domainParts[i]}`;
+      dnsList.push(domainPart);
+    }
+  });
+
+  const dnsInfoObject = {};
+  dnsList.sort();
+  for (let i = 0; i < dnsList.length;) {
+    let currentDnsCounter = 1;
+    let j = i + 1;
+    while (j < dnsList.length && dnsList[i] === dnsList[j]) {
+      currentDnsCounter++;
+      j++;
+    }
+
+    dnsInfoObject[dnsList[i]] = currentDnsCounter;
+    i = j;
+  }
+
+  return dnsInfoObject;
 }
 
 module.exports = {
