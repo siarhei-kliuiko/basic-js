6b64a82651fd3b5e42475f988b61c6fe593a6516
siarhei-kliuiko <siarhei.kliuiko@gmail.com>
2023-04-16 17:53:09 +0300

    feat: implement minesweeper function

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const MINE_AREA_SIZE = 3;
+  const mineSweeperField = [...Array(matrix.length)].map(() => Array(matrix[0].length).fill(0));
+  const increaseFieldNumber = (row, col) => {
+    if (row >= 0 && row < matrix.length && col >= 0 && col < matrix[row].length) {
+      mineSweeperField[row][col]++;
+    }
+  };
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        mineSweeperField[i][j]--;
+        for (let row = 0; row < MINE_AREA_SIZE; row++) {
+          for (let col = 0; col < MINE_AREA_SIZE; col++) {
+            increaseFieldNumber(i - 1 + row, j - 1 + col);
+          }
+        }
+      }
+    }
+  }
+
+  return mineSweeperField;
 }
 
 module.exports = {
